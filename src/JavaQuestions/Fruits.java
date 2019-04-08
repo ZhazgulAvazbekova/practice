8e00a9f21f2f65fe40e8947144039c1c8b263177
jazgul <jazgulya94@gmail.com>
2019-04-08 16:45:37 -0500

    some changes

--- src/JavaQuestions/Fruits.java
+++ src/JavaQuestions/Fruits.java
@@ -4,6 +4,7 @@ public class Fruits {
 	private void sysout() {
 		// TODO Auto-generated method stub
 
+		System.out.println("This is just for practice");
 	}
 
 }
---
+++ src/JavaQuestions/TC.java
@@ -0,0 +1,8 @@
+package JavaQuestions;
+
+public class TC {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
